a49bd0747adc8c6a6c1cb5eb3c0f050da822a43b
coderlong <862983319@qq.com>
2017-12-11 10:20:47 +0800

    add from pyCharm

--- ex01/numpy/test2.py
+++ ex01/numpy/test2.py
@@ -3,6 +3,9 @@ import  numpy as np
 
 names = np.array(['bob','asd','sailong','yake'])
 print(names)
-#data = randn(7,4)
+data = np.random.randn(7,4)
+print(data)
+data[data < 0] = 0
+print(data)
 #print(data)
 print(names == 'bob')
